102138ce209de1abeb7e07d5eaa18ef3f1a888e1
AshwaniRaiUTA <amr3939@mavs.uta.edu>
2024-07-01 18:07:03 -0500

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,18 +13,30 @@ module.exports = {
       center: true,
       padding: "15px",
     },
-      screens: {
-        sm: "640px",
-        md: "768px",
-        lg: "960px",
-        xl: "1200px",
-      },
-      fontFamily: {
-        primary: "var(--font-jetbrainsMono)",
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1200px",
+    },
+    fontFamily: {
+      primary: "var(--font-jetbrainsMono)",
 
-      },
+    },
     extend: {
       keyframes: {
+
+        colors: {
+
+          primary: '#1c1c22' ,
+          accent: {
+
+            DEFAULT: '00ff99' ,
+            hover: '#00e187' ,
+          }
+
+        } ,
+        
         "accordion-down": {
           from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
